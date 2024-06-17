54c35b50d5dfb8012018d06ede6d6d9d0a729624
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-06-17 08:01:10 +0200

    esmodule and setup

---
+++ NODEJS-CRASH-COURSE/index.js
@@ -0,0 +1,12 @@
+import  getPosts, {getLength} from './postController.js';
+
+console.log(getPosts());
+console.log(`Length is ${getLength()}`);
+
+
+// console.log('Hello World');
+
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils.js');
+
+// console.log(`Random number: ${generateRandomNumber()}`);
+// console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`);
