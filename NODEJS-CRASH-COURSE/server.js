de57a85659c8c099edbd76d7e8ee47dd3972b365
Gaurav Kumar <gauravkumar7aug92@gmail.com>
2024-06-18 06:22:50 +0200

    Request, postman

--- NODEJS-CRASH-COURSE/server.js
+++ NODEJS-CRASH-COURSE/server.js
@@ -1,14 +1,29 @@
 import http from 'http';
+//const PORT = process.env.PORT;
 const PORT = 8000;
 
 const server = http.createServer((req, res) => {
-    // res.setHeader('Content-Type', 'text/html');
-    // res.statusCode = 404;
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello World</h1>');
+    try {
+        if(req.method === 'GET') {
+            if(req.url === '/') {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<h1>Homepage</h1>');
+            } else if(req.url === '/about') {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end('<h1>About</h1>');
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/html'});
+                res.end('<h1>Not Found</h1>');
+            }
+        } else {
+            throw new Error('Method not allowed')
+        }
+    } catch(error) {
+        res.writeHead(500, {'Content-Type': 'text'});
+        res.end('Server Error');
+    }
 });
 
 server.listen(PORT, () => {
-    console.log(`Server is running on the port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
